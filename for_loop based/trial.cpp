15963e2a88e1e06b983a0c74d55f5b82fce4398c
Prath23 <111076546+Prath23@users.noreply.github.com>
2022-11-14 22:55:05 +0530

    Add files via upload

---
+++ for_loop based/trial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#include <string>
+int main(){
+	//create a visual multipicaltion tabel sheet:
+	
+	cout<<"              ********WELCOME TO TABEL GENERATOR********    "<<endl;
+	cout<<""<<endl;
+	int number;
+	cout<<"enter number upto which the multiplication sheet need to be form: ";
+	cin>>number;
+	for(int i=1;i<=number;i++){
+		cout<<""<<endl;
+		for(int a=1;a<=10;a++){
+			
+			cout<<i<<" x "<<a<<" = "<<i*a<<endl;   //has creation of cheat sheet but time complexity O(n^2)? Y/N?
+		}
+	}
+	
+	
+    system("pause>0");
+    return 0;
+}
