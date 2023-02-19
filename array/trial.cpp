35ad27301eee2074dcb30a19e124fb10cf1ea5b6
Prath23 <111076546+Prath23@users.noreply.github.com>
2023-02-19 15:16:37 +0530

    Add files via upload

---
+++ array/trial.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include<string>
+//add an elemnt in the array at the end
+class arrOPP{
+	public:
+		void addEelemnt(string arr[],int size,string f){
+			arr[size-1]=f;
+	
+		}
+};
+
+
+
+
+
+int main(){
+	string a[4]={"max","app","jhon"};
+	string miss;
+	cout<<"add the missing person name: ";
+	cin>>miss;
+	int j=sizeof(a)/sizeof(a[0]);// this is done as the sizeof() will mulltipy size with byte: 5*4
+	arrOPP addElementLast;
+	addElementLast.addEelemnt(a,j,miss);
+	for(int i=0;i<j;i++){
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
