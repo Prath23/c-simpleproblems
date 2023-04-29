173e5c08a143a40c228d1f20e7cd05bb4bd6085d
Prath23 <111076546+Prath23@users.noreply.github.com>
2023-04-29 10:26:29 +0530

    Add files via upload

---
+++ DSA/LIST/Untitled1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	list<int> a;
+	a.push_back(3);
+	a.push_back(4);
+	a.push_front(8);
+	
+	
+	
+	a.erase(a.begin()+1);
+	
+	
+	return 0;
+}
+
+
+
+
