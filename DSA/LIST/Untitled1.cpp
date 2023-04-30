5bf24a5d4a36646a721f9fb3cdfca33219a5bd41
Prath23 <tanangeprathamesh23@gmail.com>
2023-04-30 10:31:29 +0530

    Add files via upload

--- DSA/LIST/Untitled1.cpp
+++ DSA/LIST/Untitled1.cpp
@@ -2,19 +2,29 @@
 using namespace std;
 
 int main(){
-	list<int> a;
+	list <int> a;
+	a.push_front(2);
 	a.push_back(3);
 	a.push_back(4);
-	a.push_front(8);
-	
-	
-	
-	a.erase(a.begin()+1);
-	
-	
-	return 0;
+	a.push_back(5);
+	for(auto i: a){
+	    cout<<i<<"\t";
+	}
+	auto it=a.begin();
+	auto it2=a.begin();
+	advance(it,2);
+	advance(it2,1);
+	a.insert(it,88);
+	a.erase(it2);
+	for(auto i:a){
+	    if(i==5){
+	       i=10; 
+	    }
+		
+	}
+	 cout<<endl;
+	for(auto i: a){
+	   
+	    cout<<i<<"\t";
+	}
 }
-
-
-
-
