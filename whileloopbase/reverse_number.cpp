8633f0e097f8551cd09f7e1ee56c5c724fc845be
Rishabh Mathur <114498690+RishabhMathur60@users.noreply.github.com>
2023-06-11 02:27:14 +0530

    Update reverse_number.cpp

--- whileloopbase/reverse_number.cpp
+++ whileloopbase/reverse_number.cpp
@@ -4,17 +4,13 @@ using namespace std;
 int main(){
 	//revesing a given number
 	int num,c,rev=0;
-	cout<<"enter an whole number that u want to reverse: ";
+	cout<<"Enter an Whole Number That you Want To Reverse: ";
 	cin>>num;
-	
 	while(num>0){
 		c=num%10;
 		rev=(rev*10)+c;
-		num=num/10;
-		
-	}
+		num=num/10;}
 	cout<<"number"<<rev;
-	
     system("pause>0");
     return 0;
 }
