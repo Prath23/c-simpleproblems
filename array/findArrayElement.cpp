20f90e5e6417bdc02eaa07e151856e949856afba
Rishabh Mathur <114498690+RishabhMathur60@users.noreply.github.com>
2023-06-11 02:43:28 +0530

    Update findArrayElement.cpp

--- array/findArrayElement.cpp
+++ array/findArrayElement.cpp
@@ -11,24 +11,19 @@ int find(int arr[],int n,int j){
 		return -1;
 	}
 }
-
-
-
-
-
 int main(){
 	int a[]={2,5,8,4,7};
 	int s= sizeof(a);
 	int j;
-	cout<<"Enter the number that you think in the array: \n";
+	cout<<"Enter The Number That You Think In The Array: \n";
 	cin>>j;
 	int output=find(a,s,j);
 	if(output==-1){
-		cout<<"the number is not in the array"<<endl;
+		cout<<"The Number Is Not In The Array"<<endl;
 		
 	}
 	else{
-		cout<<"Element\t"<<a[output]<<" exist at index: "<<output+1<<endl;
+		cout<<"Element\t"<<a[output]<<" Exists at Index: "<<output+1<<endl;
 	}
 	return 0;
 }
